c1c1daf067408f90cbba0eb7b041cab99ccc10b3
go-tech01 <zoneofgod@gmail.com>
2021-07-09 11:21:51 +0900

    7/9 4ban class commit 1

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+# 장고에서 기본적으로 제공해주는 모델을 상속 받는다
+class NewModel(models.Model):
+    text = models.CharField(max_length=255, null=False)        ## 이text는 문자열을 받을 것이다
