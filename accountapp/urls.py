7a6a1fa157c4a779abba029b846c0e7ee92a06b2
go-tech01 <zoneofgod@gmail.com>
2021-07-14 17:49:02 +0900

    7/14 4ban class commit 2

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from accountapp.views import hello_world, AccountCreateView
 
@@ -8,6 +9,12 @@ app_name = 'accountapp' #주소를 일일이 쓰기 힘드기 때문에 설정
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+
+    # path('login/', LoginView.as_view(templates_name='accountapp/login.html'),
+    #      name='login'),
+    path('login',LoginView.as_view(template_name='accountapp/login.html'),name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
+
     # name 설정도 주소를 일일이 쓰기 힘드기 때문에 설정
     path('create/', AccountCreateView.as_view(), name='create')
 ]
