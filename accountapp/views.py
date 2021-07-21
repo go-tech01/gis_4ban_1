9ef4e04273baed943cd344739ab25d60dc553bc6
go-tech01 <zoneofgod@gmail.com>
2021-07-21 18:16:39 +0900

    7/21 4ban class commit 4

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
@@ -5,6 +6,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountCreationForm
@@ -40,20 +42,22 @@ from accountapp.models import NewModel
 #     #     # return render(request, 'accountapp/hello_world.html',
 #     #     #               context={'text':'GET METHOD!'})
 
+# @login_required(login_url=reverse_lazy('accountapp:login'))    ## accounts가 아니라면 login_url를 적어야한다
+@login_required
 def hello_world(request):
-    if request.user.is_authenticated:
-        if request.method == "POST":
-            temp = request.POST.get('input_text')
-            new_model = NewModel()
-            new_model.text = temp
-            new_model.save()
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        else:
-            data_list = NewModel.objects.all()
-            return render(request, 'accountapp/hello_world.html',
-                          context={'data_list': data_list})
+    # if request.user.is_authenticated:
+    if request.method == "POST":
+        temp = request.POST.get('input_text')
+        new_model = NewModel()
+        new_model.text = temp
+        new_model.save()
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+        data_list = NewModel.objects.all()
+        return render(request, 'accountapp/hello_world.html',
+                      context={'data_list': data_list})
+    # else:
+    #     return HttpResponseRedirect(reverse('accountapp:login'))
 
 
 class AccountCreateView(CreateView): ## generic ~!!!
@@ -67,6 +71,8 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'  # 렌더링은 필요함 -> 라우팅
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm          ## 상속을 만들어서 새로운 클래스 만들어서 회원 아이디 비활성화
@@ -74,36 +80,40 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')    # 일단...은... 나중에 detail로 ...
     template_name = 'accountapp/update.html'
 
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:  # self.get_object() = target_user
-            return super().get(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-            # return HttpResponseRedirect(reverse('accountapp:login'))
+    # 클래스 안에 함수는 메서드이기 떄문에 decorotor를 매서드에서 쓸수 있게 해야한다.
+    # @login_required
+    # def get(self, request, *args, **kwargs):
+    #     if request.user.is_authenticated and self.get_object() == request.user:  # self.get_object() = target_user
+    #         return super().get(request, *args, **kwargs)
+    #     else:
+    #         return HttpResponseForbidden()
+    #         # return HttpResponseRedirect(reverse('accountapp:login'))
+    #
+    # def post(self, request, *args, **kwargs):
+    #     if request.user.is_authenticated and self.get_object() == request.user:
+    #         return super().post(request, *args, **kwargs)
+    #     else:
+    #         return HttpResponseForbidden()
+    #         # return HttpResponseRedirect(reverse('accountapp:login'))
 
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().post(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-            # return HttpResponseRedirect(reverse('accountapp:login'))
-
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')    # 탈퇴 후 연결되는 화면
     template_name = 'accountapp/delete.html'
 
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().get(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-            # return HttpResponseRedirect(reverse('accountapp:login'))
-
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().post(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-            # return HttpResponseRedirect(reverse('accountapp:login'))
+    # def get(self, request, *args, **kwargs):
+    #     if request.user.is_authenticated and self.get_object() == request.user:
+    #         return super().get(request, *args, **kwargs)
+    #     else:
+    #         return HttpResponseForbidden()
+    #         # return HttpResponseRedirect(reverse('accountapp:login'))
+    #
+    # def post(self, request, *args, **kwargs):
+    #     if request.user.is_authenticated and self.get_object() == request.user:
+    #         return super().post(request, *args, **kwargs)
+    #     else:
+    #         return HttpResponseForbidden()
+    #         # return HttpResponseRedirect(reverse('accountapp:login'))
--- decorators.py
+++ decorators.py
@@ -1,12 +1,41 @@
-def decorator(func):
-    def decorated(input_text):
-        print('함수 시작!')
-        func(input_text)
-        print('함수 끝!')
-    return decorated
+# def decorator(func):
+#     def decorated(input_text):
+#         print('함수 시작!')
+#         func(input_text)
+#         print('함수 끝!')
+#     return decorated
+#
+# @decorator
+# def hello_world(input_text):
+#     print(input_text)
+#
+# hello_world('Hello World!!')
 
-@decorator
-def hello_world(input_text):
-    print(input_text)
+#삼각형 넓이 계산 함수
+#사각형의 넓이 계산 함수
+# 입력값이 모두 양수인지 확인하고 아닐경우 error 발생시키는 decorator 작성 및 적용
 
-hello_world('Hello World!!')
+
+def check_integer(func):
+    def dec(b, h):
+        if (b > 0) and (h > 0):
+            return func(b, h)
+        else:
+            raise ValueError('Input must be positive value')
+    return dec
+
+@check_integer
+def cal(b, h):
+    print(f'삼각형 넓이 : {b * h / 2}')
+    print(f'사각형 넓이 : {b * h}')
+
+
+cal(3, 2)
+cal(3, -2)
+
+class User:
+    def __init__(self, auth):
+        self.is_auth = auth
+
+user = User(auth=False)
+~~~~
