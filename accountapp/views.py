9bd0799525d47632f4396bc80fad6e3330b85357
go-tech01 <zoneofgod@gmail.com>
2021-07-21 16:50:53 +0900

    7/21 4ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -10,36 +10,50 @@ from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from accountapp.forms import AccountCreationForm
 from accountapp.models import NewModel
 
+# def hello_world(request):             ##request 정보가 적혀있다
+#     # 라우팅 : 어떤 주소로 요청을 보내야 해당 view를 작동시킬 것인지 - url
+#     # http://       127.0.0.1:8000/     account/hello_world/      ?name=noeul
+#     # (통신 프로토콜   서버 주소/IP          페이지 경로                쿼리 데이터)
+#     # return HttpResponse('Hello World!')
+#     # return render(request, 'base.html')
+#     if request.method == "POST":
+#
+#         temp = request.POST.get('input_text')
+#
+#         new_model = NewModel()
+#         new_model.text = temp
+#         new_model.save()
+#
+#         return HttpResponseRedirect(reverse('accountapp:hello_world'))
+#
+#         # data_list = NewModel.objects.all()
+#         # return render(request, 'accountapp/hello_world.html',  ## html를 이용한 view
+#         #               context={'data_list': data_list})
+#                         # context={'text': temp})
+#                         # context={'text':'POST METHOD!'})
+#     # elif request.method == "GET":
+#     else:
+#         data_list = NewModel.objects.all()
+#         return render(request, 'accountapp/hello_world.html',
+#                       context={'data_list': data_list})
+#
+#     #     # return render(request, 'accountapp/hello_world.html',
+#     #     #               context={'text':'GET METHOD!'})
 
-def hello_world(request):             ##request 정보가 적혀있다
-    # 라우팅 : 어떤 주소로 요청을 보내야 해당 view를 작동시킬 것인지 - url
-    # http://       127.0.0.1:8000/     account/hello_world/      ?name=noeul
-    # (통신 프로토콜   서버 주소/IP          페이지 경로                쿼리 데이터)
-    # return HttpResponse('Hello World!')
-    # return render(request, 'base.html')
-    if request.method == "POST":
-
-        temp = request.POST.get('input_text')
-
-        new_model = NewModel()
-        new_model.text = temp
-        new_model.save()
-
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-
-        # data_list = NewModel.objects.all()
-        # return render(request, 'accountapp/hello_world.html',  ## html를 이용한 view
-        #               context={'data_list': data_list})
-                        # context={'text': temp})
-                        # context={'text':'POST METHOD!'})
-    # elif request.method == "GET":
+def hello_world(request):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            temp = request.POST.get('input_text')
+            new_model = NewModel()
+            new_model.text = temp
+            new_model.save()
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        else:
+            data_list = NewModel.objects.all()
+            return render(request, 'accountapp/hello_world.html',
+                          context={'data_list': data_list})
     else:
-        data_list = NewModel.objects.all()
-        return render(request, 'accountapp/hello_world.html',
-                      context={'data_list': data_list})
-
-    #     # return render(request, 'accountapp/hello_world.html',
-    #     #               context={'text':'GET METHOD!'})
+        return HttpResponseRedirect(reverse('accountapp:login'))
 
 
 class AccountCreateView(CreateView): ## generic ~!!!
@@ -60,8 +74,32 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')    # 일단...은... 나중에 detail로 ...
     template_name = 'accountapp/update.html'
 
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().get(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().post(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
+
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')    # 탈퇴 후 연결되는 화면
     template_name = 'accountapp/delete.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().get(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().post(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp:login'))
