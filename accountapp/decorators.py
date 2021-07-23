852c1be0749afea9173f2e29cc33ed527eaf62b0
go-tech01 <zoneofgod@gmail.com>
2021-07-23 11:54:36 +0900

    7/23 4ban class commit 2

--- accountapp/decorators.py
+++ accountapp/decorators.py
@@ -1,9 +1,6 @@
-
-
 from django.contrib.auth.models import User
 from django.http import HttpResponseForbidden
 
-# 계정소유권이 필요하다
 def account_ownership_required(func):
     def decorated(request, *args, **kwargs):
         target_user = User.objects.get(pk=kwargs['pk'])
