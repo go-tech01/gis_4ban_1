615d6eb7d52d2fba946e41b9636fe24f4372df37
go-tech01 <zoneofgod@gmail.com>
2021-08-10 12:29:59 +0900

    8/10 4ban class commit 4

--- articleapp/views.py
+++ articleapp/views.py
@@ -5,12 +5,16 @@ from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
+from django.views.generic.edit import FormMixin
 
 from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
 
 # 로그인을 한 상태 정의
+from commentapp.forms import CommentCreationForm
+
+
 @method_decorator(login_required, 'get')
 @method_decorator(login_required, 'post')
 class ArticleCreateView(CreateView):
@@ -27,8 +31,9 @@ class ArticleCreateView(CreateView):
         return reverse('articleapp:detail', kwargs={'pk':self.object.pk}) # self.object = target_article
 
 # 비공개 설정같은거 할 수도 있다 -> 우선 모두 공개
-class ArticleDetailView(DetailView):
+class ArticleDetailView(DetailView, FormMixin):
     model = Article
+    form_class = CommentCreationForm
     context_object_name = 'target_article'
     template_name = 'articleapp/detail.html'
 
