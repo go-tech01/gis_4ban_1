738bf0ebe2ddcc8244a9594048c839ef7a4d005f
go-tech01 <zoneofgod@gmail.com>
2021-08-10 11:32:31 +0900

    8/10 4ban class commit 2

--- commentapp/models.py
+++ commentapp/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from articleapp.models import Article
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.SET_NULL, related_name='comment', null=True)
+    # 1대 다수 / target_article.comment
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='comment', null=True)
+    content = models.TextField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    # DateField - 일자만, DateTimeField - 시간과 날짜 전부
