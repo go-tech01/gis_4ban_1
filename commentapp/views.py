e6ff2d8782613eb3f95151c86e0ef138781b0ea9
go-tech01 <zoneofgod@gmail.com>
2021-08-11 16:41:55 +0900

    8/11 4ban class commit 1

--- commentapp/views.py
+++ commentapp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic import CreateView, DeleteView
 
+from articleapp.models import Article
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
 
@@ -13,14 +14,19 @@ class CommentCreateView(CreateView):
     form_class = CommentCreationForm
     template_name = 'commentapp/create.html'
 
+    def form_valid(self, form):
+        form.instance.writer = self.request.user
+        # form.instance.article = Article.objects.get(pk=self.request.POST.get('article_pk'))
+        form.instance.article_id = self.request.POST.get('article_pk')
+        return super().form_valid(form)
+
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk':self.object.article.pk})
 
-# class CommentDeleteView(DeleteView):
-#     model = Comment
-#     context_object_name = 'target_article.comment'   ##
-#     form_class = CommentCreationForm
-#     template_name = 'commentapp/delete.html'
-#
-#     def get_success_url(self):
-#         return reverse('articleapp:detail', kwargs={'pk':self.object.article.pk})
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk':self.object.article.pk})
