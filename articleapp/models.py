6e134f2d76e6c5f78695f0934cd09e7eb21b2954
go-tech01 <zoneofgod@gmail.com>
2021-08-27 11:37:20 +0900

    8/27 4ban class commit 2

--- articleapp/models.py
+++ articleapp/models.py
@@ -9,7 +9,7 @@ class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
     # on_delete=models.CASCADE 유저가 탈퇴하면 삭제됨, SET_NULL 게시판이 누군지 모르겠다
     # target_user.article
-    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article', null=True)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article', null=True, blank=True)
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
     content = models.TextField(null=True)
