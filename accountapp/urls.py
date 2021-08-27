5014f54c1809da4b3887e17446e7bbaec17d661c
go-tech01 <zoneofgod@gmail.com>
2021-08-27 11:15:52 +0900

    8/27 4ban class commit 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,15 +1,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
+from accountapp.views import AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accountapp' #주소를 일일이 쓰기 힘드기 때문에 설정
 
 # 127.0.0.1:800/accounts/hellow_world/ =
-# acountapp:hello_world     이런 식으로 나중에 대체해서 쓴다 - 라우티
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world'),
-
     # path('login/', LoginView.as_view(templates_name='accountapp/login.html'),
     #      name='login'),
     path('login/',LoginView.as_view(template_name='accountapp/login.html'),name='login'),
--- accountapp/views.py
+++ accountapp/views.py
@@ -14,61 +14,12 @@ from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountCreationForm
 from accountapp.models import NewModel
 
-# def hello_world(request):             ##request 정보가 적혀있다
-#     # 라우팅 : 어떤 주소로 요청을 보내야 해당 view를 작동시킬 것인지 - url
-#     # http://       127.0.0.1:8000/     account/hello_world/      ?name=noeul
-#     # (통신 프로토콜   서버 주소/IP          페이지 경로                쿼리 데이터)
-#     # return HttpResponse('Hello World!')
-#     # return render(request, 'base.html')
-#     if request.method == "POST":
-#
-#         temp = request.POST.get('input_text')
-#
-#         new_model = NewModel()
-#         new_model.text = temp
-#         new_model.save()
-#
-#         return HttpResponseRedirect(reverse('accountapp:hello_world'))
-#
-#         # data_list = NewModel.objects.all()
-#         # return render(request, 'accountapp/hello_world.html',  ## html를 이용한 view
-#         #               context={'data_list': data_list})
-#                         # context={'text': temp})
-#                         # context={'text':'POST METHOD!'})
-#     # elif request.method == "GET":
-#     else:
-#         data_list = NewModel.objects.all()
-#         return render(request, 'accountapp/hello_world.html',
-#                       context={'data_list': data_list})
-#
-#     #     # return render(request, 'accountapp/hello_world.html',
-#     #     #               context={'text':'GET METHOD!'})
-
-# @login_required(login_url=reverse_lazy('accountapp:login'))    ## accounts가 아니라면 login_url를 적어야한다
 from articleapp.models import Article
-
-
-@login_required
-def hello_world(request):
-    # if request.user.is_authenticated:
-    if request.method == "POST":
-        temp = request.POST.get('input_text')
-        new_model = NewModel()
-        new_model.text = temp
-        new_model.save()
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else:
-        data_list = NewModel.objects.all()
-        return render(request, 'accountapp/hello_world.html',
-                      context={'data_list': data_list})
-    # else:
-    #     return HttpResponseRedirect(reverse('accountapp:login'))
-
 
 class AccountCreateView(CreateView): ## generic ~!!!
     model = User                     ## 커스텀 마이징 할 수 있음
     form_class = UserCreationForm    ## 커스텀 마이징 할 수 있음
-    success_url = reverse_lazy('accountapp:hello_world')   ## 함수에서는 reverse / 클래스에서는 _lazy 를 써야함 - 불러오는 방식의 차이
+    success_url = reverse_lazy('articleapp:list')   ## 함수에서는 reverse / 클래스에서는 _lazy 를 써야함 - 불러오는 방식의 차이
     template_name = 'accountapp/create.html'
 
 
@@ -95,7 +46,6 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm          ## 상속을 만들어서 새로운 클래스 만들어서 회원 아이디 비활성화
     context_object_name = 'target_user'
-    # success_url = reverse_lazy('accountapp:hello_world')    # 일단...은... 나중에 detail로 ...
     template_name = 'accountapp/update.html'
     
     def get_success_url(self):
@@ -128,7 +78,7 @@ class AccountUpdateView(UpdateView):
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')    # 탈퇴 후 연결되는 화면
+    success_url = reverse_lazy('articleapp:list')    # 탈퇴 후 연결되는 화면
     template_name = 'accountapp/delete.html'
 
     # def get(self, request, *args, **kwargs):
